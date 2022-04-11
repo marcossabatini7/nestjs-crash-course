cad4a8e0bf32c6508e7f8e4d6574d639e027465e
Marcos Sabatini <marcossabatini7@gmail.com>
2022-04-11 17:49:10 -0300

    clean db when start test

--- nestjs-api/test/app.e2e-spec.ts
+++ nestjs-api/test/app.e2e-spec.ts
@@ -1,9 +1,12 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
+
 import { AppModule } from '../src/app.module'
+import { PrismaService } from '../src/prisma/prisma.service'
 
 describe('App e2e', () => {
   let app: INestApplication
+  let prisma: PrismaService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -17,6 +20,9 @@ describe('App e2e', () => {
       })
     )
     await app.init()
+
+    prisma = app.get(PrismaService)
+    await prisma.cleanDb()
   })
 
   afterAll(() => {
